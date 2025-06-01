594e8fa6a6810f05dfaee40310799f2f00dc1fd7
kimcodell <kimminhyuk0719@gmail.com>
2025-06-01 23:11:19 +0900

    로그인 여부에 따라 리다이렉션 기능 추가. TODO: 내정보 API 추가 후 주석해제 및 수정

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -2,8 +2,22 @@ import Container from "./Container";
 import Main from "./Main";
 import TopNav from "./TopNav";
 import Nav from "./Nav";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../store/slices/user";
+import { useNavigate } from "react-router-dom";
 
 const MainLayout = ({ children }) => {
+  //TODO 내 정보 API 추가 후 수정
+  // const isLoggedIn = useSelector(selectIsLoggedIn);
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (!isLoggedIn) {
+  //     navigate("/login");
+  //   }
+  // }, [isLoggedIn, navigate]);
+
   return (
     <Container>
       <TopNav />
